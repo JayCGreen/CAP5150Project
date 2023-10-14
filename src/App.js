0fac5c72d70f6b346ce3102c3746ed39a05ac2b7
Joseph Green <josephg896@gmail.com>
2023-10-14 11:07:02 -0400

    First commit, creation of lgin and upload page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {Button, Content, darkTheme, Flex, Provider, TextField} from '@adobe/react-spectrum'
+
 
 function App() {
+  const [loginInfo, setLoginInfo] = useState({ username:'', password:''})
+
   return (
+    <Provider theme={darkTheme}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header>Generic Company</header>
+        <h5><u>Sign in</u></h5>
+        <hl/>
+        <Content >
+          <Flex alignItems={'center'} direction={'column'} gap={'size-250'}>
+              <TextField label='Username' onChange={(text)=>setLoginInfo({...loginInfo, username: text})}></TextField>
+              <TextField label='Password' type={'password'} onChange={(text)=>setLoginInfo({...loginInfo, password: text})}></TextField>
+              <Button type='submit' onPress={()=>{
+                console.log('pressed')
+                console.log(loginInfo)
+              }}>Login
+              </Button>
+            
+          </Flex>
+        </Content>
       </header>
-    </div>
+    </div></Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MemApp from './MemberPage';
+import Upload from './uploads';
+import App from './MemberPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MemApp />
   </React.StrictMode>
 );
 
